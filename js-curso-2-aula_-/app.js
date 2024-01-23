2c8342a1d4964a1126052fb424ea9a7b587e7c64
JerryTom20 <osvaldofabarboza@gmail.com>
2024-01-22 22:07:40 -0300

    Botão clicado

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML='Jogo do numero secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um numero de 0 a 10';
+
+function verificarChute() {
+      console.log('O botão foi clicado');
+}
