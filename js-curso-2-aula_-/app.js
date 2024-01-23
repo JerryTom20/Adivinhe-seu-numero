e3109d57ff8d8f88ca2589d2d8c7b6bf559d1301
JerryTom20 <osvaldofabarboza@gmail.com>
2024-01-22 21:45:14 -0300

    Primeiro commit

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML='Jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero de 0 a 10';
