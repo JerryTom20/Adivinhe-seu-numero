48a67905ed09b679e26a1618d41b359439ff8518
JerryTom20 <osvaldofabarboza@gmail.com>
2024-01-23 22:01:40 -0300

    Edição da parte de alerta do jogo (msg na tela)

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -1,9 +1,38 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML='Jogo do numero secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML='Jogo do numero secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um numero de 0 a 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um numero de 0 a 10';
+
+
+let numeroSecreto = 2;
+
+function ProjetoNaTela(tag, texto){
+      let campo= document.querySelector(tag);
+      campo.innerHTML=texto;
+}
+
+ProjetoNaTela('h1', 'Jogo do numero secreto');
+ProjetoNaTela('p', 'Escolha um numero de 0 a 10');
+
+
 
 function verificarChute() {
-      console.log('O botão foi clicado');
+      
+      let chute = document.querySelector('input').value;
+      if (chute == numeroSecreto){
+            ProjetoNaTela('h1', 'Você acertou !')
+            ProjetoNaTela('p', 'Você acertou o numero secreto')
+      }else{ 
+            if(chute >numeroSecreto){
+                 ProjetoNaTela
+            }
+      console.log(chute === numeroSecreto);
+      
+} 
+
+function gerarNumeroAleatrio(){
+     return parseInt(Math.random()*10 + 1);
+
 }
+
